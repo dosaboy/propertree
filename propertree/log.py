82ce06ac756212210a2d07a47e9541cbd706cdd3
Edward Hope-Morley <edward.hope-morley@canonical.com>
2023-09-01 10:54:06 +0100

    Configure logging handler if none exists

--- propertree/log.py
+++ propertree/log.py
@@ -3,7 +3,9 @@ import logging
 import os
 
 log = logging.getLogger('propertree')
-log.name = 'propertree'
+format = ("%(asctime)s.%(msecs)03d %(process)d %(levelname)s %(name)s [-] "
+          "%(message)s")
+
 dbg = os.environ.get('PROPERTREE_DEBUG')
 if dbg and dbg.lower() == 'true':
     log.setLevel(logging.DEBUG)
@@ -11,3 +13,8 @@ else:
     # Force min. info level since the debug logs are very verbose and not
     # likely useful unless specifically requested.
     log.setLevel(logging.INFO)
+
+if log.level and not log.hasHandlers():
+    handler = logging.StreamHandler()
+    handler.setFormatter(logging.Formatter(format))
+    log.addHandler(handler)
